73eae1dcb73fec530553c887534d64e05f9806f1
Zac Crosby <Zacros7164@gmail.com>
2018-11-18 07:28:58 -0500

    more (failed) practice on algorithm 3

--- 11:16:18/is-prime.py
+++ 11:16:18/is-prime.py
@@ -1,11 +1,11 @@
 # in this universe, 2 and 3 are ALWAY prime
 known_primes = [2,3]
-
+finalPrime = 2
 # a function that will find if a number is prime
 def is_prime(n):
     # print n
     total_known_primes = len(known_primes)
-    for i in range(0, total_known_primes):
+    for i in xrange(0, total_known_primes):
         if(n % known_primes[i] == 0):
             # THIS IS DIVISIBLE BY A PRIME NUMBER.
             # therefore it CANNOT be a prime number
@@ -16,18 +16,28 @@ def is_prime(n):
             continue
     # we went through all KP and never hit return false, which means this must be prime
     known_primes.append(n)
-    finalPrime = known_primes[-1]
+    # finalPrime = known_primes[-1]
     if(i == total_known_primes):
         return True
 
-# print is_prime(6)
+# print is_prime(31)
 # print is_prime(8)
 # print is_prime(11)
 # print is_prime(13)
 
-
+prime_factor = []
 num = int(raw_input("number: "))
-for i in range(2, num/2+1):
+for i in xrange(2, num):
     is_prime(i)
-    
-print known_primes[-1]
+    print known_primes
+    if num % finalPrime == 0:
+            prime_factor.append(finalPrime)
+    if len(known_primes) > 10:
+        del known_primes[0]
+        
+
+    # print known_primes
+print known_primes
+print prime_factor
+
+
